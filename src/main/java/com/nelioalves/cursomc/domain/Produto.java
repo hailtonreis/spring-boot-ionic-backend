01f8fd915280bb52e3c0805accdd9cb43ab65198
Hailton Reis <hailton.reis@gmail.com>
2021-01-03 01:26:09 -0300

    Atualizacao: utilizando somente JsonIgnore

--- src/main/java/com/nelioalves/cursomc/domain/Produto.java
+++ src/main/java/com/nelioalves/cursomc/domain/Produto.java
@@ -15,7 +15,6 @@ import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 
@@ -48,7 +47,7 @@ public class Produto implements Serializable{
 		return lista;
 	}
 	
-	@JsonBackReference
+	@JsonIgnore
 	@ManyToMany
 	@JoinTable(name = "PRODUTO_CATEGORIA", 
 		joinColumns = @JoinColumn(name = "produto_id"),
@@ -127,8 +126,4 @@ public class Produto implements Serializable{
 			return false;
 		return true;
 	}
-
-
-	
-
 }
